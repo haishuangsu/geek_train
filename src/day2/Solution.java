7319ff6e8d0b4ed36c07b8f62c2f117ed79e52a7
suhaishuang <573732089@qq.com>
2019-11-28 20:52:43 +0800

    day 3 commit

--- src/day2/Solution.java
+++ src/day2/Solution.java
@@ -15,4 +15,12 @@ public class Solution {
         }
         return i;
     }
+
+    public static void main(String args[]) {
+        int[] arrs = new int[]{1,1,2,3,3,5,6};
+        int n = new Solution().removeDuplicates(arrs);
+        for (int i = 0; i < n; i++){
+            System.out.println(arrs[i]);
+        }
+    }
 }
